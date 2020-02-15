f0a22b0451f462120916966c8875549f6d12f34b
DenisDorogov <denisdorogov@yandex.ru>
2020-02-15 20:55:17 +0500

    Начал работать над делением. При стандартном подходе ошибка 180 сек.

---
+++ public/simpleNum.php
@@ -0,0 +1,24 @@
+<?php
+$number = 600851475143;
+$dividers = new SplMaxHeap();
+
+function divisibility($num, SplMaxHeap $dividers) {
+    for ($i=2; $i<=$num; $i++) {
+        if (($num % $i) == 0) {
+            $dividers->insert($num/$i);
+        }
+        if ($i % 1000000 == 0) echo $i/1000000 . '  ';
+    }
+
+
+//    foreach ($dividers as $value) {
+//        if (($num % $value) == 0) {
+//            $result[] = $num/$value;
+//        }
+
+//    }
+
+    return $result;
+}
+
+var_dump(divisibility($number, $dividers));
