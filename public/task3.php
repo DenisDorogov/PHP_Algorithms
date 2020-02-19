0e9161b4246550b818c27f3946b6e9c63463cfd7
DenisDorogov <denisdorogov@yandex.ru>
2020-02-19 18:49:22 +0500

    Продолжаю работать над двумя алгоритмами. Пока ни один не работает в полной мере.

--- public/task3.php
+++ public/task3.php
@@ -8,25 +8,30 @@ $start = 0;
 $finish = 1;
 $i = 0;
 $iterationLog = [];
-echo $countCycle % 2;
+//echo $countCycle % 2;
 
-function spiral($x, $y) {
-    global $counter,$countCycle, $start, $finish, $i;
+//Универсальная рекурсия (пока не работает)
+function spiral($x, $y)
+{
+    global $counter, $countCycle, $start, $finish, $i;
     $i -= $start;
-    for ($i ; $i<$x-$finish; ) {
+    for ($i; $i < $x - $finish;) {
         $array[$start][$i] = $counter;
         $counter++;
-        if ($countCycle % 4 < 2 ) {$i++;} else {$i--;};
+        if ($countCycle % 4 < 2) {
+            $i++;
+        } else {
+            $i--;
+        };
 //        if ($counter == 10) {
 //            var_dump($x);
 //            die;
 //        }
-
     }
 
 //    ($countCycle % 2 == 0) ? $i++ : $i--;
     $start = $x;
-    $x = $x-1;//Убираем номер последней заполненной стороны
+    $x = $x - 1;//Убираем номер последней заполненной стороны
     $countCycle++;
     if ($counter == 16) {
 
@@ -36,41 +41,57 @@ function spiral($x, $y) {
 
 }
 
-function spiralSTD($x, $y) {
-    global $array, $counter,$a, $b, $iterationLog;
-    $iterator = 0;
-    for ($i = $iterator; $i<$a-$iterator-1; $i++) {//i-столбцы,
-        $array[$iterator][$i] = $counter++; //[строка][столбец]
-        $iterationLog[] = [$i,$iterator];
+//4 цикла заполняющих периметр, + рекурсия для перехода внутрь.
+function spiralSTD($x, $y)
+{
+    global $array, $counter, $a, $b, $iterationLog, $start;
+    $j = $start;
+    $i = $start;
+    echo 'Start i='. $i. ' j='.$j . PHP_EOL. '<br>';
+    for ($i ; $i < $x - $start - 1; $i++) {//i-столбцы,
+        $array[$j][$i] = $counter++; //[строка][столбец]
+        $iterationLog[] = [$i, $start, $counter - 1]; //Отладка
     }
-    for ($j = $iterator; $j<$b-$iterator-1; $j++) {
-        $array[$j][$i] = $counter++;
-        $iterationLog[] = [$i,$j];
+    echo 'FIRST   i='. $i. ' j='.$j . PHP_EOL. '<br>';
+    $iterationLog[] = ['--', '--'];
 
-    }
-    for ($i; $i>$iterator; $i--) {
+    for ($j ; $j < $y - $start - 1; $j++) {
         $array[$j][$i] = $counter++;
-        $iterationLog[] = [$i,$j];
+        $iterationLog[] = [$i, $j, $counter - 1];
     }
-    for ($j; $j>$iterator; $j--) {
+    echo 'SECOND i='. $i. ' j='.$j . PHP_EOL. '<br>';
+    $iterationLog[] = ['--', '--'];
+
+    for ($i; $i > $start; $i--) {
         $array[$j][$i] = $counter++;
-        $iterationLog[] = [$i,$j];
+        $iterationLog[] = [$i, $j, $counter - 1];
     }
+    echo 'THIRD  i='. $i. ' j='.$j . PHP_EOL. '<br>';
+    $iterationLog[] = ['--', '--'];
+
+    for ($j; $j > $start+1; $j--) {
+        $array[$j][$i] = $counter++;
+        $iterationLog[] = [$i, $j, $counter - 1];
+    }
+    echo 'FOURTH i='. $i. ' j='.$j . PHP_EOL . '<br><br>';
+
+    $start = $start + 1;
+    $iterationLog[] = ['==', '==', $start];
 
 
-    if ($counter <= $a*$b)  {
-        $iterator++;
-        spiralSTD($x, $y);
+    if ($counter < 20) {
+        spiralSTD($y, $x);
     }
 }
 
-function render($array) {
+function render($array)
+{
     global $a, $b;
     $count = count($array);
     echo '<table>';
-    for ($i= 0; $i < $count+1; $i++) {
+    for ($i = 0; $i < $count + 1; $i++) {
         echo '<tr>';
-        for ($j= 0; $j < count($array[$i]); $j++) {
+        for ($j = 0; $j < count($array[$i]); $j++) {
             echo "<td width='20px'> {$array[$i][$j]} </td>";
         }
         echo '</tr>';
@@ -79,9 +100,9 @@ function render($array) {
 
 }
 
-$start = microtime(true);
-spiral($a, $b);
-echo  microtime(true) - $start;
+//$start = microtime(true);
+spiralSTD($a, $b);
+//echo microtime(true) - $start;
 
 render($array);
-//render($iterationLog);
+render($iterationLog);
