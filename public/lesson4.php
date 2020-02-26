b881a1cf6ec482e4b84983a6edba12d658007ff4
DenisDorogov <denisdorogov@yandex.ru>
2020-02-26 10:34:33 +0500

    Продолжаем работу над задачей

--- public/lesson4.php
+++ public/lesson4.php
@@ -1,4 +1,38 @@
 <?php
 $file = new SplFileObject('numbers.txt');
 $file->seek(0);
-echo $file->current();
+$num1 = str_split($file->current());
+$file->seek(1);
+$num2 = str_split($file->current());
+
+function largeSum($n1, $n2)
+{
+    $result = '';
+    $shift = 0;
+    $count1 = 5;
+    $count2 = 0;
+    if (count($n1) < count($n2)) {
+        $temp = $n1;
+        $n1 = $n2;
+        $n2 = $temp;
+        $count1 = count($n1);
+        $count2 = count($n2);
+    }
+    var_dump($count1);
+//    for ($i = $count1; $i >= 0; $i--) {
+//        $sum = $n1[$i] + $n2[$i];
+//        if ($sum > 9) {
+//            $result = $result . (string)(($sum - 10) + $shift);
+//            $shift = 1;
+//        } else {
+//            $result = $result . (string)($sum + $shift);
+//            $shift = 0;
+//        }
+//    }
+    var_dump($result);
+}
+
+largeSum($num1, $num2);
+
+//var_dump($num1);
+//var_dump($num2);
